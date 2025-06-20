8f9d122e68ab1d89b70a4ae3223c66c9484dce51
ocmisrael <orcohen2411@gmail.com>
2025-06-20 17:46:33 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 async function scrapeYad2(url) {
   const results = [];
   const { data } = await axios.get(url, {
-    headers: { "User-Agent": "Mozilla/5.0" }
+    headers: {   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" }
   });
 
   const $ = cheerio.load(data);
